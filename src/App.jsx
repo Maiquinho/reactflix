2816c11704ff76829d31430f3ea7f695a3311e56
Michael Matheus <michaelmatheus2001@gmail.com>
2023-03-18 18:01:04 -0300

    feat: homepage overview

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { MovieRow } from './components/MovieRow';
+import Tmdb from './Tmdb';
+
+
+export default function App (){
+
+    const [movieList, setMovieList] = useState([]);
+
+    useEffect(() => {
+
+        async function loadAll(){
+            // Getting all returned data
+            let list = await Tmdb.getHomeList();
+            setMovieList(list);
+            console.log(list);
+        }
+
+        loadAll();
+
+    }, []);
+
+
+    return (
+        <div className="page">
+            <section className="movie-lists">
+                { movieList.map((item, key) => (
+                    <MovieRow key={key} title={item.title} items={item.items} />
+                )) }
+            </section>
+        </div>
+    )
+}
